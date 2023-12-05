6725573d0809dc0fbac36c5285f2447cea6853c7
Alejandro Buccini <alejandrobuccini@gmail.com>
2023-12-04 21:07:05 -0300

    actualize todo con mi logica pero con el front de jesus

--- proyecto_nuevo/templates/msg.js
+++
@@ -1,7 +0,0 @@
-function confirmarEliminacion() {
-    var confirmacion = confirm("¿Estás seguro de que deseas eliminar los datos de este usuario?");
-    if (confirmacion) {
-      document.getElementById("formEliminar").submit();
-    } else {
-    }
-  }
